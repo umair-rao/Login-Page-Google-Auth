69ec4ea7f2d82e1abdd504aa3891a7bc68028ce1
Umair Rao <umairmuqeem@gmail.com>
2023-09-09 19:38:24 +0500

    Add Upload images functionalities

--- app/uploadFile/page.jsx
+++ app/uploadFile/page.jsx
@@ -1,8 +1,12 @@
+import Link from "next/link"
 
 const page = () => {
   return (
     <div className="pl-10 pt-5">
-      <h1>Upload your file section will be updated tommorow</h1>
+      <div className="flex justify-evenly w-full">
+        <Link href="/upload-button">Upload Button</Link>
+        <Link href="/upload-dnd">Upload Drag n Drop</Link>
+      </div>
     </div>
   )
 }
