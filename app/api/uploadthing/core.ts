03e527d548f8a5e2d45eebb374c9be1aded46720
Umair Rao <umairmuqeem@gmail.com>
2023-09-11 13:51:06 +0500

    Upload PDF funcationality updated

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -30,11 +30,30 @@ export const ourFileRouter = {
 
     // Takes a 4 2mb images and/or 1 256mb video
   mediaPost: f({
-    image: { maxFileSize: "2MB", maxFileCount: 4 },
+    image: { maxFileSize: "2MB", maxFileCount: 10 },
     video: { maxFileSize: "256MB", maxFileCount: 1 },
   })
   .middleware(({ req }) => auth(req))
   .onUploadComplete((data) => console.log("file", data)),
+
+    // Define a new FileRoute for PDF files with a unique routeSlug
+    pdfUploader: f({ pdf: { maxFileSize: "32MB", maxFileCount: 5 } }) // Adjust maxFileSize and maxFileCount as needed
+    .middleware(async ({ req }) => {
+      // This code runs on your server before upload
+      const user = await auth(req);
+
+      // If you throw, the user will not be able to upload
+      if (!user) throw new Error("Unauthorized");
+
+      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      // This code RUNS ON YOUR SERVER after upload
+      console.log("Upload complete for userId:", metadata.userId);
+
+      console.log("file url", file.url);
+    }),
 } satisfies FileRouter;
  
 export type OurFileRouter = typeof ourFileRouter;
--- app/upload-dnd/page.tsx
+++ app/upload-dnd/page.tsx
@@ -1,60 +1,104 @@
 "use client";
- 
-// You need to import our styles for the button to look right. Best to import in the root /layout.tsx but this is fine
+
 import "@uploadthing/react/styles.css";
- 
 import { UploadDropzone } from "@uploadthing/react";
 import { OurFileRouter } from "../api/uploadthing/core";
-import { useState } from 'react'
+import { useState } from "react";
 import Link from "next/link";
- 
+
 export default function UploadButtonPage() {
-  const [images, setImages] = useState<{
-    fileUrl: string;
-    fileKey: string;
-  }[]>([])
+  const [images, setImages] = useState<
+    {
+      fileUrl: string;
+      fileKey: string;
+    }[]
+  >([]);
 
-const title = images.length ? (
-  <>
-  <p>Upload Complete</p>
-  <p className="mt-2">{images.length} File(s) Uploaded</p>
-  </>
-): null
+  const [pdfs, setPDFs] = useState<
+    {
+      fileUrl: string;
+      fileKey: string;
+    }[]
+  >([]);
 
-const imgList =(
-  <>
-  {title}
-  <ul>
-    {images.map(image => (
-      <li key={image.fileUrl} className="mt-2">
-        <Link href={image.fileUrl} target="_blank">
-          {image.fileUrl}
-        </Link>
-      </li>
-    ))}
-  </ul>
-  </>
-)
+  const titleImages = images.length ? (
+    <>
+      <p>Image Upload Complete</p>
+      <p className="mt-2">{images.length} Files Uploaded</p>
+    </>
+  ) : null;
+
+  const titlePDFs = pdfs.length ? (
+    <>
+      <p>PDF Upload Complete</p>
+      <p className="mt-2">{pdfs.length} Files Uploaded</p>
+    </>
+  ) : null;
+
+  const imgList = (
+    <>
+      {titleImages}
+      <ul>
+        {images.map((image) => (
+          <li key={image.fileUrl} className="mt-2">
+            <Link href={image.fileUrl} target="_blank">
+              {image.fileUrl}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+
+  const pdfList = (
+    <>
+      {titlePDFs}
+      <ul>
+        {pdfs.map((pdf) => (
+          <li key={pdf.fileUrl} className="mt-2">
+            <Link href={pdf.fileUrl} target="_blank">
+              {pdf.fileUrl}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start p-24">
-      <UploadDropzone<OurFileRouter>
-        endpoint="imageUploader"
-        onClientUploadComplete={(res) => {
-          if(res) {
-            setImages(res)
-            const json = JSON.stringify(res)
-          // Do something with the response
-          console.log(json);
-          }
-          // alert("Upload Completed");
-        }}
-        onUploadError={(error: Error) => {
-          // Do something with the error.
-          alert(`ERROR! ${error.message}`);
-        }}
-      />
-      {imgList}
+    <main className="flex min-h-screen flex-row items-start justify-center p-24">
+      <div>
+        <UploadDropzone<OurFileRouter>
+          endpoint="imageUploader"
+          onClientUploadComplete={(res) => {
+            if (res) {
+              setImages(res);
+              const json = JSON.stringify(res);
+              console.log(json);
+            }
+          }}
+          onUploadError={(error: Error) => {
+            alert(`ERROR! ${error.message}`);
+          }}
+        />
+        {imgList}
+      </div>
+      <div className="ml-10">
+        <UploadDropzone<OurFileRouter>
+          endpoint="pdfUploader" // Use the appropriate endpoint for PDF files
+          onClientUploadComplete={(res) => {
+            if (res) {
+              setPDFs(res);
+              const json = JSON.stringify(res);
+              console.log(json);
+            }
+          }}
+          onUploadError={(error: Error) => {
+            alert(`ERROR! ${error.message}`);
+          }}
+        />
+        {pdfList}
+      </div>
     </main>
   );
 }
